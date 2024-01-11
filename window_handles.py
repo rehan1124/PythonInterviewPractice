08d7e9bd567859cda2f96a0789f27b78c26699a4
srehan1 <syed.rehan@shoppertrak.com>
2024-01-11 14:26:50 +0530

    Window_Handles

--- window_handles.py
+++ window_handles.py
@@ -1,17 +1,33 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+
+class AppLoadError(Exception):
+    pass
+
 
 URL = "https://demo.automationtesting.in/Windows.html"
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.implicitly_wait(30)
+driver.implicitly_wait(15)
 driver.get(URL)
 
-driver.find_element(By.CSS_SELECTOR, "[id='Tabbed'] button[class='btn btn-info']").click()
+current_window = driver.current_window_handle
 
-handles = driver.window_handles
-print("Windows opened:", handles)
-for items in handles:
-    driver.switch_to.window(items)
-    print(driver.title)
+try:
+    driver.find_element(By.CSS_SELECTOR, "[id='Tabbed'] button[class='btn btn-inf']").click()
+
+    handles = driver.window_handles
+    print("Windows opened:", handles)
+    for items in handles:
+        driver.switch_to.window(items)
+        print(driver.title)
+except Exception:
+    raise AppLoadError
+finally:
+    driver.switch_to.window(current_window)
+    time.sleep(10)
+    driver.quit()
