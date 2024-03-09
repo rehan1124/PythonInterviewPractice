13f27286d0253e36867f8c3e8669fa1a2424cfcf
rehan1124 <rehan.11.24@gmail.com>
2024-03-09 23:37:34 +0530

    Python design patterns

--- design_patterns/design_patterns.py
+++ design_patterns/design_patterns.py
@@ -1,6 +1,14 @@
 """
 Design patterns using Python
 
-https://www.youtube.com/watch?v=qpq8DOecwDs&list=PL9ok7C7Yn9A8ewGj22MbLOyD9VCtZn7lF
+https://www.youtube.com/watch?v=nCBtBcGQT7A
+
+https://www.youtube.com/watch?v=qpq8DOecwDs&list=PL9ok7C7Yn9A8ewGj22MbLOyD9VCtZn7lF JAVA
 https://www.youtube.com/watch?v=wNiMoNiQQJg
+"""
+
+"""
+Creational - All about class and object instantiation
+Structural - Organize different class and objects to form large structures/new functionality
+Behavioral - Common communication between objects
 """
